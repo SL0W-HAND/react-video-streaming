41186fd7611b89d765f1d2335f6ff2c22417221c
SL0W-HAND <daniel.carrete.guzman@gmail.com>
2021-05-09 13:33:35 -0500

    player settings

--- src/pages/Player.js
+++ src/pages/Player.js
@@ -1,17 +1,20 @@
 import React, { Component } from 'react';
 import serverIp from '../ipConfig.js';
+import {connect} from 'react-redux';
+import {Rdirect, Redirect } from 'react-router-dom'
 import Navbar from '../components/Navbar'
 
-export default class Player extends Component {
+class Player extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            videoId: this.props.match.params.id,
+            videoId: null,
             videoData: {}
         };
     }
     async componentDidMount() {
         try {
+            this.setState({videoId:this.props.match.params.id})
             const res = await fetch(`http://${serverIp}:4000/video/${this.state.videoId}/data`);
             const data = await res.json();
             this.setState({ videoData: data });
@@ -25,8 +28,15 @@ export default class Player extends Component {
                 <video controls muted autoPlay>
                     <source src={`http://${serverIp}:4000/video/${this.state.videoId}`} type="video/mp4"></source>
                 </video>
+                <div className='player-black'>
+                <button type='button' onClick={()=> this.props.history.goBack()}>
+                    Back
+                </button>
+                </div>
                 <h1>{ this.state.videoData.name }</h1>
             </>
         )
     }
 }
+
+export default connect(null,null)(Player)
